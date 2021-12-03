9c7296385a46618df7d8970106861f786154f1cf
Dan Vo <vtthanhdan.qn@gmail.com>
2021-12-03 11:51:49 -0700

    update signup

--- pages/signup.js
+++ pages/signup.js
@@ -6,6 +6,7 @@ import { UserLogin } from "../components/authentication/user-login"
 import { PageLayout, PageHeader, PageBody } from "../layouts/loginpage"
 import { Button } from "../ui/buttons"
 import TextInput from "../ui/forms/TextInput"
+import GitHubProvider from "../components/authentication/providers/github/GitHubProvider"
 
 function signup(){
 
@@ -18,6 +19,7 @@ function signup(){
                     </PageHeader>
                <PageBody>
                    <GoogleProvider>With Google</GoogleProvider>
+                   <GitHubProvider>With GitHub</GitHubProvider>
                    <Spacer>OR</Spacer>
                    <TextInput label="Email"  id="emailAddress" placeholder="janedoe@home.com" />
                     <TextInput label="Password"  type="password" id="emailAddress" placeholder="use a secure password" />
